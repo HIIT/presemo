1e61b206ceac6b419382a40a7772c6ff6be4da15
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-11-16 17:32:56 +0200

    Remove kissa-koira debug

--- app/blocktypes/chat/client/Message.jsx
+++ app/blocktypes/chat/client/Message.jsx
@@ -1,18 +1,9 @@
 var Message = React.createClass({
-  displayName: "Message",
 
   render: function render() {
 
-    var p = "kissa";
-
-    if( __CONTROL__ ) {
-      p = "koira";
-    }
-
-
     return <div>
       {this.props.message.text}
-      {p}
     </div>;
   }
 });
