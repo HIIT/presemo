11ea9f8e0c496b6eea6bda2b45ceea8b5e0094fe
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-11-16 17:39:59 +0200

    Add timestamps

--- app/blocktypes/chat/client/Message.jsx
+++ app/blocktypes/chat/client/Message.jsx
@@ -2,7 +2,10 @@ var Message = React.createClass({
 
   render: function render() {
 
+    var time = new Date( this.props.message.time );
+
     return <div>
+      {time.getHours()}:{time.getMinutes()} -{' '}
       {this.props.message.text}
     </div>;
   }
